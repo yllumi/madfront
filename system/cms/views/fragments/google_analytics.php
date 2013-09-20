46d4f42afb2be69bf2d9ae964eafab7bc5b5c5c3
Toni Haryanto <toha.samba@gmail.com>
2013-09-20 15:35:19 +0700

    change google analytics tracking code to new format

--- system/cms/views/fragments/google_analytics.php
+++ system/cms/views/fragments/google_analytics.php
@@ -1,15 +1,13 @@
 <?php if ($this->settings->ga_tracking): ?>
 
-<script type="text/javascript">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '<?php echo $this->settings->ga_tracking;?>'], ['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
-  })();
+  ga('create', '<?php echo $this->settings->ga_tracking;?>', '<?php echo $_SERVER['SERVER_NAME'];?>');
+  ga('send', 'pageview');
 
 </script>
 
